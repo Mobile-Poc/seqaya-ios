e7180375146a94b3fb5be8638d2b76f0183cbe3a
Mariam Elsayed Ismail <mariamismail@desktop-s8qabhd.ntgeg.com>
2018-04-11 14:14:23 +0200

    kl

--- UI/OrderSummary/Presentation/pOrderSummaryPresentation.swift
+++
@@ -1,9 +0,0 @@
-//
-//  pOrderSummaryPresentation.swift
-//  seqayaPoc
-//
-//  Created by Guest on 2018-04-04.
-//  Copyright © 2018 Mariam Elsayed Ismail. All rights reserved.
-//
-
-import Foundation
